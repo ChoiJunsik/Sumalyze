a4edab63be7a34eefb6723837e492a1e85154e35
Inho <limcara@nate.com>
2019-05-26 19:10:28 +0900

    결과페이지 이미지 크롤링 코드 추가

---
+++ sumalyze/crowling.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from bs4 import BeautifulSoup
+import urllib.request
+from urllib import parse
+
+def image_crowling(request):
+    word = '고양이'
+    tmp = 'https://ko.wikipedia.org/wiki/' + parse.quote(word)
+    
+    html = urllib.request.urlopen(tmp)
+    source = html.read()
+    soup = BeautifulSoup(source, "html.parser")
+    img = soup.find("a","image")
+    img = img.find("img")
+    
+    img_src = img.get("src")
+    img_url = 'https:' + img_src
+
+    #urllib.request.urlretrieve(img_url, "./img/" + '당근')
+    # return render(request, 'sumalyze/image_crowling.html', {
+    #     'url' : img_url,
+    # })
+    return img_src
